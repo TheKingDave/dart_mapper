459d034a9bc4071b26b974d2296236732a0c1098
David Langheiter <david@langheiter.com>
2020-05-27 20:08:18 +0200

    Init commit

---
+++ example/dart_mapper_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_mapper/dart_mapper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_mapper_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_mapper/dart_mapper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
